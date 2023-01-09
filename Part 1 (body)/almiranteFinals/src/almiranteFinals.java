59da05e1452c0bf31286ca93a34631bb95366e6b
Kevin <mamadaddyalexa@gmail.com>
2023-01-10 03:53:26 +0800

    New badge

--- Part 1 (body)/almiranteFinals/src/almiranteFinals.java
+++ Part 1 (body)/almiranteFinals/src/almiranteFinals.java
@@ -55,6 +55,7 @@ public class almiranteFinals extends Applet {
 		//c6
 		g.fillOval(1770,160,160,100);
 		//yolo
+		//trying to get new badge
 		//house1 (left-right)
 		g.setColor(new Color(32, 9, 7));
 		g.fillRect(50,580,150,270);
